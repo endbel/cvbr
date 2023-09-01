b7ef43ca64ac415d80d26985acd38232625c7cfe
Bel <etad.ivlo@gmail.com>
2023-08-31 21:54:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "Bel FrontEnd"
+    let b = miString + " Developer"
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "CV"
+
+    console.log(a)
+}
